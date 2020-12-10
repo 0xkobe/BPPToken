25130fbf9d768c3e3260ef9bcb703aa5f4dea289
devstar <dev_star11@outlook.com>
2020-12-11 01:43:40 +1000

    token name change

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
 const SimpleToken = artifacts.require("./SimpleToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleToken, "Bitpayer", "BPP", 18, 100000000);
+  deployer.deploy(SimpleToken, "Bitpower", "BPP", 18, 100000000);
 };
